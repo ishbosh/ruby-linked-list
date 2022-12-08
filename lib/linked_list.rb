c5aa327ae8ad3680cce888577e67e0d73a1d29d3
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-12-07 19:18:37 -0700

    Initial file creation

---
+++ lib/linked_list.rb
@@ -0,0 +1,18 @@
+class LinkedList
+  #append(value) method
+  #prepend(value) method
+  #size method
+  #head method
+  #tail method
+  #at(index) method
+  #pop method
+  #contains?(value) method
+  #find(value) method
+  #to_s method
+
+end
+
+class Node
+#value method
+#link to the next node
+end
