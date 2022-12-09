7e2cd57ec7a77ad695e2c069add427cc4f7e9b38
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-12-08 20:23:16 -0700

    Change value to attr_accessor

--- lib/node.rb
+++ lib/node.rb
@@ -2,8 +2,7 @@
 
 # Class for creating Node objects
 class Node
-  attr_reader :value
-  attr_accessor :next_node
+  attr_accessor :value, :next_node
 
   def initialize(value = nil, next_node = nil)
     @value = value
